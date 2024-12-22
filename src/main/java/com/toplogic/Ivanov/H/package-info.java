d4e47e3504481abe17c731c5106dee8e99f23a86
hiiico <hiiico332@gmail.com>
2024-12-22 16:01:13 +0200

    Initial Commit for this project

---
+++ src/main/java/com/toplogic/Ivanov/H/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Custom extensions for Ivanov.H.
+ */
+package com.toplogic.Ivanov.H;
+
---
+++ src/test/java/com/toplogic/Ivanov/H/TestApp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 hiiico. All Rights Reserved
+ */
+package com.toplogic.Ivanov.H;
+
+import junit.framework.TestCase;
+
+/**
+ * Test case for the app <code>Ivanov.H</code>. 
+ */
+public class TestApp extends TestCase {
+	
+	public void testIt() {
+		// Add some test.
+	}
+	
+}
